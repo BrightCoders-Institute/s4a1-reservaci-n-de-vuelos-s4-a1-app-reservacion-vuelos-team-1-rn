ca1e6371704958ead14806f7e4eda0a76896be72
Oliver Visoso <ovisosodev@gmail.com>
2024-01-30 18:26:02 -0600

    add styles for buttons on all screens

---
+++ ReservacionDeVuelos/src/theme/globalStyle.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  buttonDisable: {
+    borderRadius: 10,
+    padding: 15,
+    backgroundColor: '#b6b7ba',
+    justifyContent: 'center',
+  },
+  buttonEnable: {
+    borderRadius: 10,
+    padding: 15,
+    justifyContent: 'center',
+    backgroundColor: '#626de7',
+  },
+});
