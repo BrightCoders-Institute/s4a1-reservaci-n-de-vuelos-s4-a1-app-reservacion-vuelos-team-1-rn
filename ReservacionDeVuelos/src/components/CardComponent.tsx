6cb4b11f5cb5e1c124f192d297ac9d918730b55a
Oliver Visoso <ovisosodev@gmail.com>
2024-01-30 18:29:47 -0600

    deleted props that we weren't using
    Co-authored-by: Vanessa-Covarrubias-Nava <Vanessa.covarrubias.nava@gmail.com>
    Co-authored-by: Jherrera11 <jherrera11@ucol.mx>
    Co-authored-by: eCuevasCeballos <ecuevasceballos@gmail.com>

--- ReservacionDeVuelos/src/components/CardComponent.tsx
+++ ReservacionDeVuelos/src/components/CardComponent.tsx
@@ -1,10 +1,4 @@
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  TextStyle,
-} from 'react-native';
+import {View, TouchableOpacity, StyleSheet} from 'react-native';
 import React from 'react';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 
